38ce666258d2ac6460a4ed59af3276390ec173cb
fjavier02 <franciscolopez1995@gmail.com>
2020-11-23 23:30:47 -0300

    margin para sliderbar
    adicionando margin y responcividad para la barra

--- src/app/components/listTasks/listTasks.component.ts
+++ src/app/components/listTasks/listTasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../models/Taks';
-import { TaskService } from '../../services/task.service'
+import { TaskService } from '../../services/task.service';
 import Swal from 'sweetalert2'
 
 @Component({
