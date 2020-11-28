4518d80cb656943d0d21c12c0e3c6b24db1515c0
fjavier02 <franciscolopez1995@gmail.com>
2020-11-27 23:28:22 -0300

    Boton de cancelar edit

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -11,6 +11,12 @@ export class TasksComponent implements OnInit {
 
   myTask
   taskFound
+  oldDescriptionValue: string 
+  oldTitleValue: string
+  oldTempoValue: string
+  oldDistanciaValue: string
+  oldAlturaValue: string
+  oldDificuldadeValue: string
 
   constructor(private listServ: TaskService) { }
 
@@ -19,7 +25,16 @@ export class TasksComponent implements OnInit {
 
   getPositions($event) {
     this.myTask = this.listServ.getOneTask($event)
-    this.taskFound = this.myTask
+    this.taskFound = this.myTask;
+    console.log(this.taskFound);
+    console.log(this.myTask);
+    this.oldTitleValue = this.taskFound.title;
+    this.oldDescriptionValue = this.taskFound.description;
+    this.oldTempoValue = this.taskFound.tempo;
+    this.oldDistanciaValue = this.taskFound.distancia;
+    this.oldAlturaValue = this.taskFound.altura;
+    this.oldDificuldadeValue = this.taskFound.dificuldade;
+
   }
 
   editTask() {
@@ -32,6 +47,12 @@ export class TasksComponent implements OnInit {
     //console.log("funcionando!")
   }
   cancelEdit() {
-    console.log("cancelando el beta")// Falta la función para que no guarde los cambios
+    //console.log("cancelando el beta");
+    this.taskFound.title = this.oldTitleValue;
+    this.taskFound.description = this.oldDescriptionValue;
+    this.taskFound.tempo = this.oldTempoValue;
+    this.taskFound.distancia = this.oldDistanciaValue;
+    this.taskFound.altura = this.oldAlturaValue;
+    this.taskFound.dificuldade = this.oldDificuldadeValue;
   }
 }
