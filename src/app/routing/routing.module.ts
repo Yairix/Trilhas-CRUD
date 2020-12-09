00d724c6aa57d875b3a2ea939d727507d4ea5711
fjavier02 <franciscolopez1995@gmail.com>
2020-12-08 22:04:01 -0300

    checkpoint 2

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './../home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  // { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
